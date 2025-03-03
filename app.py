748a1acf000506d71ca8c54ce8cc88694d364ffb
Shubham Harkare <shubhamharkare@Shubhams-MacBook-Air.local>
2025-03-03 22:22:23 +0530

    Updated frontend and API's

--- app.py
+++ app.py
@@ -19,14 +19,20 @@ def homepage():
 @app.route('/predict_api',methods = ['GET','POST'])
 def predict_api():
     data = request.json['data']
-    # print(data.values())
-    # data = np.array(list(data.values)).reshape(1,-1)
     scaled_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
     output = regmodel.predict(scaled_data)
     print(output[0])
     return jsonify({'output': output[0]})
-    # return jsonify(output)
 
+
+# TODO: Write a api to get data from form
+@app.route("/predict",methods = ['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template('home.html',prediction_text = f'The predictied house price is: {output}')
 
 if __name__ == '__main__':
     app.run(debug = True)
